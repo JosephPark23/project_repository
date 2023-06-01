882d284df598729765ecadd6bcb9b45637b6e18d
JosephPark23 <134462315+JosephPark23@users.noreply.github.com>
2023-05-31 22:06:11 -0400

    Updated war code
    -Added in more functions for game logic
    -Extended play function
    -imported some libraries for changes I'm going to implement later

--- war.py
+++ war.py
@@ -1,9 +1,10 @@
 from os import system, name
 import time
+import random
 
 deck = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2, "A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2, "A", "K",
         "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2, "A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
-
+face_cards = {"A": 14, "K": 13, "Q": 12, "J": 11}
 
 def clear():
     # windows
@@ -21,12 +22,41 @@ class War:
         self.player_deck = []
         self.computer_deck = []
 
-    def deck_shuffle(self, deck_):
-        return deck_.shuffle()
+    def deck_shuffle(self):
+        random.shuffle(self.deck)
 
     def deal(self):
-        self.player_deck = [self.player_deck.append(self.deck[i]) for i in range(26)]
-        self.computer_deck = [self.computer_deck.append(self.deck[i]) for i in range(26, 52)]
+        for i in range(26):
+            self.player_deck.append(self.deck[i])
+            self.computer_deck.append(self.deck[i + 26])
+
+    def convert_facecard(self, card):
+        if card in face_cards:
+            return face_cards[card]
+        else:
+            return card
+
+    def compare_cards(self, player_card, computer_card):
+        return player_card > computer_card
+
+    def war(self, turn):
+        player_war = self.player_deck[turn:turn + 4]
+        computer_war = self.computer_deck[turn:turn + 4]
+        war_length = min(len(player_war), len(computer_war))
+        if war_length >= 4:
+            player_war_values = [self.convert_facecard(card) for card in player_war]
+            computer_war_values = [self.convert_facecard(card) for card in computer_war]
+            if player_war_values[war_length - 1] > computer_war_values[war_length - 1]:
+                self.player_deck.extend(computer_war[:war_length])
+                del self.computer_deck[turn:turn + war_length]
+            elif player_war_values[war_length - 1] < computer_war_values[war_length - 1]:
+                self.computer_deck.extend(player_war[:war_length])
+                del self.player_deck[turn:turn + war_length]
+        else:
+            if len(player_war) < 4:
+                del self.player_deck[turn:]
+            if len(computer_war) < 4:
+                del self.computer_deck[turn:]
 
     def check(self):
         if len(self.computer_deck) < 1:
@@ -39,5 +69,24 @@ class War:
             return True
 
     def play(self):
+        self.deck_shuffle()
+        self.deal()
+        turn = 0
+        print(f"Your deck: {self.player_deck}\nComputer: {self.computer_deck}")
         while True:
-            check()
+            self.check()
+            player_card = self.convert_facecard(self.player_deck[turn])
+            comp_card = self.convert_facecard(self.computer_deck[turn])
+            print(player_card, comp_card)
+            if player_card != comp_card:
+                if self.compare_cards(player_card, comp_card):
+                    self.player_deck.append(comp_card)
+                elif not self.compare_cards(player_card, comp_card):
+                    self.computer_deck.append(player_card)
+            elif player_card == comp_card:
+                self.war(turn)
+            turn += 1
+
+
+w = War()
+w.play()
