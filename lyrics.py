02274a2066b49c8b763b8005a5d818dc206ea4d0
joseph <134462315+JosephPark23@users.noreply.github.com>
2023-08-06 12:51:07 -0400

    modularized the program

--- lyrics.py
+++ lyrics.py
@@ -1,5 +1,7 @@
-# Note: Do not use this program for commercial use. Personal use only.
-# Imports necessary modules: bs4 and urllib for webscraping, os and time for cleaning up interface
+# Note: Do not use this program for commercial use, personal use only
+# Imports necessary modules: bs4 and urllib for webscraping, os and time for cleaning up interface, string and colorama
+# and utilities for aesthetics
+# Run inside terminal
 from bs4 import BeautifulSoup
 import urllib
 from urllib.request import urlopen
@@ -45,55 +47,79 @@ def tokenize(artist, song):
     return artist_name, song_name
 
 
-# initialize user input and url
+# intitializes the url
+def get_url():
+    song_name = input("what's the name of the song?: ")
+    artist_name = input("what's the primary artist's name? (if it doesn't work, try excluding the word 'the'): ")
+    artist_name, song_name = tokenize(artist_name, song_name)
+    url = f"https://www.azlyrics.com/lyrics/{artist_name}/{song_name}.html"
+    return url
+
+
+# opens and reads the lyrics page
+def get_object(url):
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    x = soup.find(class_="col-xs-12 col-lg-8 text-center")
+    a = str(x.get_text())
+    return a
+
+
+# prints lyrics in specific format
+def format_lyrics(lyrics):
+    # isolates the lyrics from the rest of the text
+    pos_one = lyrics.find("Lyrics")
+    pos_two = lyrics.find("Submit Corrections")  # print lyrics only
+    x = lyrics[pos_one + 11:pos_two - 18]
+    print(util.bold + "\n==========================\n||hey, it's your lyrics!||\n==========================\n\n")
+
+    # prints the lyrics line-by-line
+    lines = x.splitlines()
+    for line in lines:
+        print(f"{cr.Fore.CYAN}{line}")
+        time.sleep(0.1)
+
+    # checks if user wishes to look up another song
+    y = input(util.bold + "\n=============(scroll up for your lyrics!)=============\nif u wanna find another "
+                          "song, just hit that enter key!\nif u don't, that's okay! press 'q', "
+                          "then enter!\n======================================================\n")
+    return y
+
+
+# checks if user wants to leave or not
+def decision(x):
+    if x == 'q':
+        clear()
+        print(util.bold + "hey hey hey goodbye~")
+        time.sleep(2)
+        quit()
+    else:
+        clear()
+
+
+# handles the urllib HTTP error
+def handle_exception():
+    print("\n\ncan't find what u want, maybe cuz:\n1. ur spelling sucks\n2. sometimes the artist of the song "
+          "is not who u\n"
+          "think it is, or the song name is wrong\n3. this program isn't all-encompassing")
+    time.sleep(8)
+    clear()
+    open_page()
+
+
+# runs the program
 def open_page():
     print(util.bold + "welcome to my lyrics finder! look up your lyrics here!\n")
     while True:
-        song_name = input("what's the name of the song?: ")
-        artist_name = input("what's the artist/band's name? (if it doesn't work, try excluding the word 'the'): ")
-        artist_name, song_name = tokenize(artist_name, song_name)
-        url = f"https://www.azlyrics.com/lyrics/{artist_name}/{song_name}.html"
-
         try:
-            # opens and reads the lyrics page
-            page = urlopen(url)
-            html = page.read().decode("utf-8")
-            soup = BeautifulSoup(html, "html.parser")
-            x = soup.find(class_="col-xs-12 col-lg-8 text-center")
-            a = str(x.get_text())
-
-            # isolates the lyrics from the rest of the text
-            pos_one = a.find("Lyrics")
-            pos_two = a.find("Submit Corrections")  # print lyrics only
-            b = a[pos_one + 11:pos_two - 18]
-            print(util.bold + "\n==========================\n||hey, it's your lyrics!||\n==========================\n\n")
-
-            # prints the lyrics line-by-line
-            lines = b.splitlines()
-            for line in lines:
-                print(f"{cr.Fore.CYAN}{line}")
-                time.sleep(1)
-
-            # checks if user wishes to look up another song
-            q = input(util.bold + "\n=============(scroll up for your lyrics!)=============\nif u wanna find another "
-                                  "song, just hit that enter key!\nif u don't, that's okay! press 'q', "
-                                  "then enter!\n======================================================\n")
-            if q == 'q':
-                clear()
-                print(util.bold + "hey hey hey goodbye~")
-                time.sleep(2)
-                quit()
-            else:
-                clear()
+            url = get_url()
+            lyrics = get_object(url)
+            decision(format_lyrics(lyrics))
 
         # exception handling for typos and other spelling mistakes, and to keep the loop running smoothly
         except urllib.request.HTTPError:
-            print("\n\ncan't find what u want, maybe cuz:\n1. ur spelling sucks\n2. sometimes the artist of the song "
-                  "is not who u\n"
-                  "think it is, or the song name is wrong\n3. this program isn't all-encompassing")
-            time.sleep(8)
-            clear()
-            open_page()
+            handle_exception()
 
 
 open_page()
