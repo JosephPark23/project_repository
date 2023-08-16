14f7333f4c602e2a6ed7721f9afb3ef969bd0a16
joseph <134462315+JosephPark23@users.noreply.github.com>
2023-08-15 23:46:29 -0400

    Update sunrisetime.py

--- sunrisetime.py
+++ sunrisetime.py
@@ -5,22 +5,27 @@ from time import sleep
 from pyfirmata import util
 import pyfirmata as pyf
 
-# initialize board
-board = pyf.Arduino('COM5')
-it = pyf.util.Iterator(board)
-it.start()
 
-# initialize output
-motor = 13
-board.digital[motor].mode = pyf.OUTPUT
+# sets up board
+def main():
+    # initialize board
+    board = pyf.Arduino('COM5')
+    it = pyf.util.Iterator(board)
+    it.start()
 
-# creates log
-try:
-    f = open("arduino_log.txt", "x")
-    f.write("ARDUINO LOG: ")
-    f.close()
-except FileExistsError:
-    print('file already exists')
+    # initialize output
+    motor_pin = 8
+    board.digital[motor_pin].mode = pyf.OUTPUT
+
+    # creates log
+    try:
+        f = open("arduino_log.txt", "x")
+        f.write("ARDUINO LOG: ")
+        f.close()
+    except FileExistsError:
+        print('file already exists')
+
+    activate_arduino(motor_pin, board)
 
 
 # scrape sunrise and sunset times
@@ -39,25 +44,33 @@ def get_object():
     return sunrise_time_, sunset_time_
 
 
-while True:
-    # formats the returned times
-    sunrise_time_str, sunset_time_str = get_object()
-    unformatted_time = dt.strptime(sunrise_time_str, "%I:%M")
-    sunrise_time = unformatted_time.strftime("%H:%M")
-    unformatted_time_ = dt.strptime(sunset_time_str, "%H:%M")
-    sunset_time = unformatted_time_.strftime("%H:%M")
+def activate_arduino(motorPin, board):
+    while True:
+        try:
+            # formats the returned times
+            sunrise_time_str, sunset_time_str = get_object()
+            unformatted_time = dt.strptime(sunrise_time_str, "%I:%M")
+            sunrise_time = unformatted_time.strftime("%H:%M")
+            unformatted_time_ = dt.strptime(sunset_time_str, "%H:%M")
+            sunset_time = unformatted_time_.strftime("%H:%M")
 
-    # checks if time matches sunset time
-    now = dt.now()
-    current_time = now.strftime("%H:%M")
-    print(current_time)
-    print(sunset_time)
-    if current_time == sunset_time:
-        # activates the board
-        board.digital[motor].write(1)
-        sleep(4)
-        board.digital[motor].write(0)
-        log_date = str(dt.now())
-        f = open("arduino_log.txt", "a")
-        f.write(f"System activated at: {log_date}")
-        f.close()
+            # checks if time matches sunset time
+            now = dt.now()
+            current_time = now.strftime("%H:%M")
+            print(sunrise_time)
+            if current_time == sunrise_time:
+                # activates the board
+                board.digital[motorPin].write(1)
+                sleep(10)
+                board.digital[motorPin].write(0)
+                log_date = str(dt.now())
+                print('ran at', log_date)
+                x = open("arduino_log.txt", "a")
+                x.write(f"System activated at: {log_date}")
+                x.close()
+        except KeyboardInterrupt:
+            print('program stopped via manual override')
+            quit()
+
+
+main()
