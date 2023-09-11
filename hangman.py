1536ad9217b74d9865e336d71a02b2527a1262ab
joseph <134462315+JosephPark23@users.noreply.github.com>
2023-09-11 19:59:03 -0400

    Update hangman.py
    fixed bug that cut out all words after a word length of a multiple of 16

--- hangman.py
+++ hangman.py
@@ -1,7 +1,9 @@
+from datetime import datetime as dt
 from os import system, name
-import time
+from time import sleep
 
 
+# for cleaning up the screen
 def clear():
     # windows
     if name == 'nt':
@@ -11,70 +13,123 @@ def clear():
         _ = system('clear')
 
 
-class Hangman:
+# creates the blog file or handles the exception if blog file already exists
+def create_blog():
+    try:
+        f = open("blog.txt", "x")
+        f.write("My Personal Python Blog: ")
+        f.close()
 
-    # initializes variables
-    def __init__(self):
-        self.word = word
-        self.wrong_guesses = 0
-        self.guessed_so_far = ['_']*len(self.word)
-        self.positions = []
-
-    # checks if letter user enters is actually a letter
-    def validate(self, user_guess):
-        if user_guess.isalpha() and len(user_guess) == 1:
-            return True
-        else:
-            print("You did not enter a letter.")
-            return False
-
-    # checks if the letter user guessed is in the word and records position and state
-    def check(self, user_guess):
-        self.positions = []
-        for i, char in enumerate(self.word):
-            if char == user_guess:
-                self.positions.append(i)
-        return bool(self.positions)
-
-    # ends the game to see if person won
-    def end_game(self, guessed_so_far, wrong_guesses):
-        if "_" not in guessed_so_far:
-            print(f"Congratulations! You won! The word was {self.word}")
-            quit()
-        elif wrong_guesses == 8:
-        print(f"You lost, the man was hanged :(. The word was {self.word}.")
-            quit()
-
-    # play function
-        def play_hangman(self):
-            while self.wrong_guesses < 7:
-                time.sleep(3)
-                clear()
-                print(' '.join(self.guessed_so_far))
-                print(f"\nNumber of wrong guesses left: {7-self.wrong_guesses}\n")
-                self.end_game(self.guessed_so_far, self.wrong_guesses)
-                user_guess = input('Enter your guess: ')
-                if not self.validate(user_guess):
-                    continue
-                if not self.check(user_guess):
-                    print('The letter that you entered is not in the word.')
-                    self.wrong_guesses += 1
-                    continue
-                print("The letter is in the word!")
-                for pos in self.positions:
-                    self.guessed_so_far[pos] = user_guess
-        print(f"You lost, the man was hanged :(. The word was {self.word}.")
+    # after first use
+    except FileExistsError:
+        print("Accessing blog")
+        sleep(2)
+        for i in range(3):
+            sleep(1)
+            print('...')
+        clear()
 
 
+# creates a new entry
+def new_entry():
+    # records the date, time, title and entry
+    now = dt.now()
+    dt_string = now.strftime("Date: %B %d, %Y \nTime: %I:%M %p")
+    title_text = input("Enter the entry title: ")
+    entry_text = input("Enter your text: ")
 
-print("Welcome to Hangman! For instructions, go to www.wikihow.com/Play-Hangman.")
-time.sleep(5)
-while True:
+    # saves the data in the blog.txt file
+    for i in range(3):
+        sleep(0.5)
+        print('\n...')
     clear()
-    word = input("Enter a word: ")
-    if word.isalpha():
-        break
-    else:
-        print('You entered something other than a word. Try again.')
-h = Hangman()
-h.play_hangman()
+    f = open('blog.txt', 'a')
+    f.write(f"{format_entry(entry_text, title_text, dt_string)}")
+    f.close()
+
+
+# formats the entry
+def format_entry(entry_text, title_text, dt_string):
+    # variables for formatting
+    x = '#' * 90
+    return_text = []
+    current_line = []
+    words = entry_text.split()
+
+    # creates "margins" to make the text appear more organized
+    if len(words) <= 16:
+        entry_text = f"\n\n====[ {title_text} ]====\n\n{entry_text}\n\n{dt_string}\n\n{x}"
+        return entry_text
+    
+    for word in words:
+        if len(current_line) < 16:
+            current_line.append(word)
+        else:
+            line = ' '.join(current_line)
+            return_text.append(f'{line}')
+            current_line = []
+
+    if current_line:
+        line = ' '.join(current_line)
+        return_text.append(f'{line}')
+
+    # adds finishing touches and returns the formatted text
+    return_text = '\n'.join(return_text)
+    entry_text = f"\n\n====[ {title_text} ]====\n\n{return_text}\n\n{dt_string}\n\n{x}"
+    return entry_text
+
+
+# opens and prints the contents of the blog
+def open_blog():
+    f = open("blog.txt")
+    print(f.read())
+    input("\n======================\npress enter to proceed\n======================\n")
+    clear()
+
+
+# finds specific post by title
+def access_post_():
+    title = input('enter title: ')
+    clear()
+
+    # finds and prints the post
+    f = open("blog.txt")
+    blog_object = str(f.read())
+    index_one = blog_object.find(f'====[ {title} ]====')
+    if index_one == - 1:
+        print("post not found")
+        sleep(3)
+        clear()
+        return
+    index_two = blog_object.find('###', index_one+6+len(title))
+    print(f"post that you wanted is here:\n{blog_object[index_one:index_two]}")
+    input("\n======================\npress enter to proceed\n======================\n")
+    clear()
+
+
+# deletes blog post by title
+def delete_post():
+    title = input('enter title: ')
+    clear()
+
+
+
+
+
+def main():
+    create_blog()
+    while True:
+        choice = int(input("what you wanna do?\n(1) write a new entry\n(2) read the blog\n(3) access a blog post by "
+                           "title\nenter your choice: "))
+        if choice == 1:
+            clear()
+            new_entry()
+        elif choice == 2:
+            clear()
+            open_blog()
+        elif choice == 3:
+            clear()
+            access_post_()
+
+
+main()
