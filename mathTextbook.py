cada7d918bd6f9d1489abc019428583780ed2e54
joseph <134462315+JosephPark23@users.noreply.github.com>
2024-04-16 21:40:45 -0400

    Update mathTextbook.py

--- mathTextbook.py
+++ mathTextbook.py
@@ -3,7 +3,7 @@ import os
 
 # set (absolute) paths of necessary files
 acrobatPath = os.path.abspath("C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe")
-textbookPath = os.path.abspath("C:\\Users\\josep\\Downloads\\Algebra_2_and_Trigonometry.pdf")
+textbookPath = os.path.abspath("path/to/your/file")
 
 # input preprocessing
 x = int(input("enter the page: "))
